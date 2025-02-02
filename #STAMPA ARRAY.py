6ae89eafe3655332c0f05d94f0908eba5ac1a98d
Viola De Franco <defrancoviola@gmail.com>
2025-02-02 10:10:38 +0100

    Add files via upload

---
+++ #STAMPA ARRAY.py
@@ -0,0 +1,12 @@
+#STAMPA ARRAY
+
+def main():
+    # Definizione e istanziazione dell'array
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    # Stampa di un valore per riga
+    for value in array:
+        print(value)
+
+if __name__ == "__main__":
+    main()
